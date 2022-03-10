da9ea0d8f4003f399753d1c0168ab27088d49066
Przemysław Wieczorek <pwieczorek@psi.pl>
2022-03-10 08:24:27 +0100

    test

---
+++ readme.md
@@ -0,0 +1,3 @@
+https://www.spriters-resource.com/pc_computer/heroes3/ - ikonki z gry http://www.heroesofmightandmagic.com/heroes3/creaturescastle.shtml - opis jednostek, czarów itd. http://www.heroesofmightandmagic.com/heroes3/democheat.shtml - demo do ściągnięcia.
+
+command to start gui: mvn clean install && mvn javafx@debug:run -pl battle-gui
--- src/test/java/CreatureTest.java
+++ src/test/java/CreatureTest.java
@@ -27,4 +27,21 @@ public class CreatureTest
         // then
         assertThat( dragon.getCurrentHp() ).isEqualTo( 60 );
     }
+
+    @Test
+    void creatureShouldNotHealCreatureEvenHasLowerAttackThanDefenderArmor()
+    {
+        final Creature angel = new Creature.Bulider().hp( NOT_IMPORTANT )
+            .attack( 1 )
+            .defence( NOT_IMPORTANT )
+            .bulid();
+        final Creature dragon = new Creature.Bulider().hp( 100 )
+            .attack( NOT_IMPORTANT )
+            .defence( 10 )
+            .bulid();
+        // when
+        angel.attack( dragon );
+        // then
+        assertThat( dragon.getCurrentHp() ).isEqualTo( 100 );
+    }
 }
