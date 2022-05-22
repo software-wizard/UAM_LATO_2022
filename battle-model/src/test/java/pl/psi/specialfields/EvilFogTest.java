dca1eaeda91e6f17c99a5793a1fe943d17c7f384
Jankaz2 <jkazmierczak.it@gmail.com>
2022-05-22 21:24:13 +0200

    update tests with new interface

--- battle-model/src/test/java/pl/psi/specialfields/EvilFogTest.java
+++ battle-model/src/test/java/pl/psi/specialfields/EvilFogTest.java
@@ -2,6 +2,7 @@ package pl.psi.specialfields;
 
 import org.junit.jupiter.api.Test;
 import pl.psi.Point;
+import pl.psi.creatures.Alignment;
 import pl.psi.creatures.Creature;
 import pl.psi.creatures.CreatureStats;
 
@@ -13,37 +14,73 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class EvilFogTest {
 
     @Test
+    void shouldGiveOneMoraleToBadAlignedCreature() {
+        // given
+        var evilFog = new EvilFog(new Point(10, 10));
+        var badAlignedCreature = new Creature.Builder()
+                .statistic(
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.EVIL).build();
+
+        // when
+        evilFog.buffCreature(badAlignedCreature);
+
+        // then
+        assertEquals(2, badAlignedCreature.getMorale());
+    }
+
+    @Test
+    void shouldReduceOneMoraleFromGoodAlignedCreature() {
+        // given
+        var evilFog = new EvilFog(new Point(10, 10));
+        var goodAlignedCreature = new Creature.Builder()
+                .statistic(
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.GOOD).build();
+
+        // when
+        evilFog.buffCreature(goodAlignedCreature);
+
+        // then
+        assertEquals(0, goodAlignedCreature.getMorale());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenGivenCreatureIsNull() {
+        // given
+        var evilFog = new EvilFog(new Point(10, 10));
+
+        // when
+        var exception = assertThrows(IllegalArgumentException.class, () -> evilFog.buffCreature(null));
+
+        // then
+        assertEquals("Creature must not be null", exception.getMessage());
+    }
+
+    @Test
     void shouldGiveOneMoraleToAllBadAlignedCreaturesAndTakeOneMoraleFromAllGoodAlignedCreatures() {
 
         // given
         var evilFog = new EvilFog(new Point(10, 10));
         var goodAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(true)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.GOOD).build();
 
         var secondGoodAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(true)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.GOOD).build();
 
         var badAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(false)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.EVIL).build();
 
         var secondBadAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(false)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.EVIL).build();
 
         var creatures = List.of(
                 goodAlignedCreature,
@@ -54,7 +91,7 @@ public class EvilFogTest {
         );
 
         // when
-        evilFog.putBuffOnAllCreatures(creatures);
+        evilFog.buffCreatures(creatures);
 
         // then
         assertEquals(0, goodAlignedCreature.getMorale());
@@ -70,8 +107,8 @@ public class EvilFogTest {
         var evilFog = new EvilFog(new Point(10, 10));
 
         // when
-        var exceptionWhenGivenDataIsNull = assertThrows(IllegalArgumentException.class, () -> evilFog.putBuffOnAllCreatures(null));
-        var exceptionWhenGivenDataIsEmpty = assertThrows(IllegalArgumentException.class, () -> evilFog.putBuffOnAllCreatures(List.of()));
+        var exceptionWhenGivenDataIsNull = assertThrows(IllegalArgumentException.class, () -> evilFog.buffCreatures(null));
+        var exceptionWhenGivenDataIsEmpty = assertThrows(IllegalArgumentException.class, () -> evilFog.buffCreatures(List.of()));
 
         // then
         assertEquals("Creatures list must not be null", exceptionWhenGivenDataIsNull.getMessage());
--- battle-model/src/test/java/pl/psi/specialfields/HolyGroundTest.java
+++ battle-model/src/test/java/pl/psi/specialfields/HolyGroundTest.java
@@ -2,6 +2,7 @@ package pl.psi.specialfields;
 
 import org.junit.jupiter.api.Test;
 import pl.psi.Point;
+import pl.psi.creatures.Alignment;
 import pl.psi.creatures.Creature;
 import pl.psi.creatures.CreatureStats;
 
@@ -13,37 +14,73 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class HolyGroundTest {
 
     @Test
+    void shouldGiveOneMoraleToGoodAlignedCreature() {
+        // given
+        var holyGround = new HolyGround(new Point(10, 10));
+        var goodAlignedCreature = new Creature.Builder()
+                .statistic(
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.GOOD).build();
+
+        // when
+        holyGround.buffCreature(goodAlignedCreature);
+
+        // then
+        assertEquals(2, goodAlignedCreature.getMorale());
+    }
+
+    @Test
+    void shouldTakeOneMoraleFromBadAlignedCreature() {
+        // given
+        var holyGround = new HolyGround(new Point(10, 10));
+        var badAlignedCreature = new Creature.Builder()
+                .statistic(
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.EVIL).build();
+
+        // when
+        holyGround.buffCreature(badAlignedCreature);
+
+        // then
+        assertEquals(0, badAlignedCreature.getMorale());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenGivenCreatureIsNull() {
+        // given
+        var holyGround = new HolyGround(new Point(10, 10));
+
+        // when
+        var exception = assertThrows(IllegalArgumentException.class, () -> holyGround.buffCreature(null));
+
+        // then
+        assertEquals("Creature must not be null", exception.getMessage());
+    }
+
+    @Test
     void shouldGiveOneMoraleToAllGoodAlignedCreaturesAndTakeOneMoraleFromAllBadAlignedCreatures() {
 
         // given
         var holyGround = new HolyGround(new Point(10, 10));
         var goodAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(true)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.GOOD).build();
 
         var secondGoodAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(true)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.GOOD).build();
 
         var badAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(false)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.EVIL).build();
 
         var secondBadAlignedCreature = new Creature.Builder()
                 .statistic(
-                        CreatureStats.builder()
-                                .isGoodAligned(false)
-                                .build()
-                ).build();
+                        CreatureStats.builder().build()
+                ).alignment(Alignment.EVIL).build();
 
         var creatures = List.of(
                 goodAlignedCreature,
@@ -54,7 +91,7 @@ public class HolyGroundTest {
         );
 
         // when
-        holyGround.putBuffOnAllCreatures(creatures);
+        holyGround.buffCreatures(creatures);
 
         // then
         assertEquals(2, goodAlignedCreature.getMorale());
@@ -70,8 +107,8 @@ public class HolyGroundTest {
         var holyGround = new HolyGround(new Point(10, 10));
 
         // when
-        var exceptionWhenGivenDataIsNull = assertThrows(IllegalArgumentException.class, () -> holyGround.putBuffOnAllCreatures(null));
-        var exceptionWhenGivenDataIsEmpty = assertThrows(IllegalArgumentException.class, () -> holyGround.putBuffOnAllCreatures(List.of()));
+        var exceptionWhenGivenDataIsNull = assertThrows(IllegalArgumentException.class, () -> holyGround.buffCreatures(null));
+        var exceptionWhenGivenDataIsEmpty = assertThrows(IllegalArgumentException.class, () -> holyGround.buffCreatures(List.of()));
 
         // then
         assertEquals("Creatures list must not be null", exceptionWhenGivenDataIsNull.getMessage());
