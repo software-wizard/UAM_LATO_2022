f73c88c318c1bc51365c2a5f196d05d6dbce7b90
Software-Wizard <przemek@szkola-dobrego-kodu.pl>
2022-05-24 20:19:55 +0200

    Merge with fraction

--- battle-model/src/main/java/pl/psi/creatures/AbstractCreature.java
+++ battle-model/src/main/java/pl/psi/creatures/AbstractCreature.java
@@ -4,103 +4,87 @@ import com.google.common.collect.Range;
 
 import java.beans.PropertyChangeEvent;
 
-import com.google.common.collect.Range;
-import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.Getter;
 
-import java.beans.PropertyChangeEvent;
 
-public class AbstractCreature extends Creature
-{
+public class AbstractCreature extends Creature {
+    @Getter(AccessLevel.PROTECTED)
     private final Creature decorated;
 
-    public AbstractCreature( final Creature aDecorated )
-    {
+    public AbstractCreature(final Creature aDecorated) {
         decorated = aDecorated;
     }
 
     @Override
-    public CreatureStatisticIf getBasicStats()
-    {
+    public CreatureStatisticIf getBasicStats() {
         return decorated.getBasicStats();
     }
 
     @Override
-    public CreatureStatisticIf getStats()
-    {
+    public CreatureStatisticIf getStats() {
         return decorated.getStats();
     }
 
     @Override
-    public int getAmount()
-    {
+    public int getAmount() {
         return decorated.getAmount();
     }
 
     @Override
-    public DamageCalculatorIf getCalculator()
-    {
+    public DamageCalculatorIf getCalculator() {
         return decorated.getCalculator();
     }
 
     @Override
-    public void attack( final Creature aDefender )
-    {
-        decorated.attack( aDefender );
+    public void attack(final Creature aDefender) {
+        decorated.attack(aDefender);
     }
 
     @Override
-    public boolean isAlive()
-    {
+    public boolean isAlive() {
         return decorated.isAlive();
     }
 
 
     @Override
-    public void heal( final double healAmount)
-    {
-        decorated.heal( healAmount ) ;
+    public void heal(final double healAmount) {
+        decorated.heal(healAmount);
     }
 
     @Override
-    public double getCurrentHp()
-    {
+    public double getCurrentHp() {
         return decorated.getCurrentHp();
     }
 
     @Override
-    protected void setCurrentHp( final double aCurrentHp )
-    {
-        decorated.setCurrentHp( aCurrentHp );
+    protected void setCurrentHp(final double aCurrentHp) {
+        decorated.setCurrentHp(aCurrentHp);
     }
 
     @Override
-    Range< Integer > getDamage()
-    {
+    Range<Integer> getDamage() {
         return decorated.getDamage();
     }
 
     @Override
-    double getAttack()
-    {
+    double getAttack() {
         return decorated.getAttack();
     }
 
     @Override
-    double getArmor()
-    {
+    double getArmor() {
         return decorated.getArmor();
     }
 
     @Override
-    protected void restoreCurrentHpToMax()
-    {
+    protected void restoreCurrentHpToMax() {
         decorated.restoreCurrentHpToMax();
     }
 
     @Override
-    public void propertyChange( final PropertyChangeEvent evt )
-    {
-        decorated.propertyChange( evt );
+    public void propertyChange(final PropertyChangeEvent evt) {
+        decorated.propertyChange(evt);
     }
 
 }
--- economy-model/src/main/java/pl/psi/creatures/EconomyCastleFactory.java
+++ economy-model/src/main/java/pl/psi/creatures/EconomyCastleFactory.java
@@ -2,41 +2,34 @@ package pl.psi.creatures;
 
 import pl.psi.shop.Money;
 
-public class EconomyCastleFactory
-{
+public class EconomyCastleFactory {
 
     private static final String EXCEPTION_MESSAGE = "We support tiers from 1 to 7";
 
-    public EconomyCreature create( final boolean aIsUpgraded, final int aTier, final int aAmount )
-    {
-        if( !aIsUpgraded )
-        {
-            switch( aTier )
-            {
+    public EconomyCreature create(final boolean aIsUpgraded, final int aTier, final int aAmount) {
+        if (!aIsUpgraded) {
+            switch (aTier) {
                 case 1:
-                    return new EconomyCreature( CreatureStatistic.PIKEMAN, aAmount, 60 );
+                    return new EconomyCreature(CreatureStatistic.PIKEMAN, aAmount, new Money(60));
                 case 2:
                     // TODO
                     // add archer
-                    return new EconomyCreature( CreatureStatistic.GRIFFIN, aAmount, 200 );
+                    return new EconomyCreature(CreatureStatistic.GRIFFIN, aAmount, new Money(200));
                 case 3:
-                    return new EconomyCreature( CreatureStatistic.SWORDSMAN, aAmount, 300 );
+                    return new EconomyCreature(CreatureStatistic.SWORDSMAN, aAmount, new Money(300));
                 default:
-                    throw new IllegalArgumentException( EXCEPTION_MESSAGE );
+                    throw new IllegalArgumentException(EXCEPTION_MESSAGE);
             }
-        }
-        else
-        {
-            switch( aTier )
-            {
+        } else {
+            switch (aTier) {
                 case 1:
-                    return new EconomyCreature( CreatureStatistic.HALBERDIER, aAmount, 75 );
+                    return new EconomyCreature(CreatureStatistic.HALBERDIER, aAmount, new Money(75));
                 case 2:
-                    return new EconomyCreature( CreatureStatistic.ROYAL_GRIFFIN, aAmount, 240 );
+                    return new EconomyCreature(CreatureStatistic.ROYAL_GRIFFIN, aAmount, new Money(240));
                 case 3:
-                    return new EconomyCreature( CreatureStatistic.CRUSADER, aAmount, 400 );
+                    return new EconomyCreature(CreatureStatistic.CRUSADER, aAmount, new Money(400));
                 default:
-                    throw new IllegalArgumentException( EXCEPTION_MESSAGE );
+                    throw new IllegalArgumentException(EXCEPTION_MESSAGE);
             }
         }
     }
