ad50aa9e032d98806468a5906acc56c4028f885a
AWieczarek <adamwie12@gmail.com>
2022-06-05 23:24:05 +0200

    Added magic class and name enum

--- battle-model/src/main/java/pl/psi/spells/AreaDamageSpell.java
+++ battle-model/src/main/java/pl/psi/spells/AreaDamageSpell.java
@@ -11,8 +11,8 @@ public class AreaDamageSpell extends Spell<List<Creature>> {
     private final boolean[][] area;
     private final int value;
 
-    public AreaDamageSpell(SpellTypes category, String name, SpellRang rang, int manaCost, boolean[][] area, int value) {
-        super(category, name, rang, manaCost);
+    public AreaDamageSpell(SpellTypes category, SpellNames name, SpellMagicClass spellMagicClass, SpellRang rang, int manaCost, boolean[][] area, int value) {
+        super(category, name, spellMagicClass, rang, manaCost);
         this.area = area;
         this.value = value;
     }
