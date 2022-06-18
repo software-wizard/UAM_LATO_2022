1be31e60651bb5f751bcc289236b829b9a6b81d6
mikmum <cobblechups@gmail.com>
2022-06-18 17:41:29 +0200

    added artficat applier for hero skills

--- economy-model/src/main/java/pl/psi/artifacts/ArtifactApplier.java
+++ economy-model/src/main/java/pl/psi/artifacts/ArtifactApplier.java
@@ -4,6 +4,8 @@ import pl.psi.artifacts.model.ArtifactEffect;
 import pl.psi.artifacts.model.ArtifactTarget;
 import pl.psi.creatures.CreatureStatisticIf;
 import pl.psi.creatures.CreatureStats;
+import pl.psi.hero.HeroStatisticsIf;
+import pl.psi.hero.HeroStats;
 
 /**
  * Service responsible for taking care of calculating upgraded values after applying artifact.
@@ -46,6 +48,48 @@ public class ArtifactApplier {
             default:
                 throw new UnsupportedOperationException("Unrecognised applying target type.");
         }
+    }
+
+    public HeroStatisticsIf calculateHeroUpgradedStatisticsAfterApplyingArtifact(
+            final ArtifactEffect<? extends ArtifactEffectApplicable> aArtifactEffect, final HeroStatisticsIf aBaseStats) {
+
+        if ( !( aArtifactEffect.getApplierTarget().getArtifactTarget() == ArtifactTarget.SKILL) ) {
+            throw new IllegalArgumentException("Artifact Effect is not meant to be applied to Hero's skill.");
+        }
+
+        final SkillArtifactApplicableProperty applierTarget = (SkillArtifactApplicableProperty) aArtifactEffect.getApplierTarget();
+
+        final HeroStats.HeroStatsBuilder heroStatsBuilder = HeroStats.builder()
+                .name(aBaseStats.getName())
+                .attack(aBaseStats.getAttack())
+                .defence(aBaseStats.getDefence())
+                .spellPower(aBaseStats.getSpellPower())
+                .knowledge(aBaseStats.getKnowledge())
+                .morale(aBaseStats.getMorale())
+                .luck(aBaseStats.getLuck())
+                .spellPoints(aBaseStats.getSpellPoints());
+
+        switch (applierTarget) {
+            case ATTACK:
+                final int upgradedAttack = aArtifactEffect.calculateStatisticValueAfterApplying(
+                        new ArtifactEffectApplyingProperties(aBaseStats.getAttack(), aBaseStats.getAttack()));
+                return heroStatsBuilder.attack(upgradedAttack).build();
+            case DEFENCE:
+                final int upgradedDefence = aArtifactEffect.calculateStatisticValueAfterApplying(
+                        new ArtifactEffectApplyingProperties(aBaseStats.getDefence(), aBaseStats.getDefence()));
+                return heroStatsBuilder.defence(upgradedDefence).build();
+            case KNOWLEDGE:
+                final int upgradedKnowledge = aArtifactEffect.calculateStatisticValueAfterApplying(
+                        new ArtifactEffectApplyingProperties(aBaseStats.getKnowledge(), aBaseStats.getKnowledge()));
+                return heroStatsBuilder.knowledge(upgradedKnowledge).build();
+            case SPELL_POWER:
+                final int upgradedSpellPower = aArtifactEffect.calculateStatisticValueAfterApplying(
+                        new ArtifactEffectApplyingProperties(aBaseStats.getSpellPower(), aBaseStats.getSpellPower()));
+                return heroStatsBuilder.spellPower(upgradedSpellPower).build();
+            default:
+                throw new UnsupportedOperationException("Unrecognised applying target type.");
+        }
+
     }
 
     // TODO: Two new methods for calculating upgraded skills & spells.
