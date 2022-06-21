2ab3a3635f687d7d145a8fbb6e34eb26576c36dc
korzepadawid <korzepadawid@yahoo.com>
2022-06-21 20:39:11 +0200

    tests: applying skill artifact effects

--- economy-model/src/test/java/pl/psi/artifacts/ArtifactApplierTest.java
+++ economy-model/src/test/java/pl/psi/artifacts/ArtifactApplierTest.java
@@ -2,11 +2,20 @@ package pl.psi.artifacts;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.psi.artifacts.model.ArtifactApplyingMode;
 import pl.psi.artifacts.model.ArtifactEffect;
+import pl.psi.hero.HeroStatistics;
+import pl.psi.hero.HeroStatisticsIf;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArtifactApplierTest {
+
+    private static final double MULTIPLIER = 2.1;
+    public static final double VALUE_TO_ADD = 3;
 
     private ArtifactApplier artifactApplier;
 
@@ -21,9 +30,7 @@ class ArtifactApplierTest {
                 .applierTarget(SkillArtifactApplicableProperty.ATTACK)
                 .build();
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            artifactApplier.calculateCreatureUpgradedStatisticAfterApplyingArtifact(effect, null, null);
-        });
+        assertThrows(IllegalArgumentException.class, () -> artifactApplier.calculateCreatureUpgradedStatisticAfterApplyingArtifact(effect, null, null));
     }
 
     @Test
@@ -32,9 +39,7 @@ class ArtifactApplierTest {
                 .applierTarget(CreatureArtifactApplicableProperty.ATTACK)
                 .build();
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            artifactApplier.calculateHeroUpgradedStatisticsAfterApplyingArtifact(effect, null);
-        });
+        assertThrows(IllegalArgumentException.class, () -> artifactApplier.calculateHeroUpgradedStatisticsAfterApplyingArtifact(effect, null));
     }
 
     @Test
@@ -43,8 +48,38 @@ class ArtifactApplierTest {
                 .applierTarget(SkillArtifactApplicableProperty.ATTACK)
                 .build();
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            artifactApplier.calculateSpellFactorsModifiers(effect, null);
-        });
+        assertThrows(IllegalArgumentException.class, () -> artifactApplier.calculateSpellFactorsModifiers(effect, null));
+    }
+
+    @Test
+    void shouldUpgradeHeroStatisticsCorrectlyWhenMultiplying() {
+        // given
+        final ArtifactEffect<ArtifactEffectApplicable> effect = ArtifactEffect.builder()
+                .effectValue(BigDecimal.valueOf(MULTIPLIER))
+                .effectApplyingMode(ArtifactApplyingMode.MULTIPLY)
+                .applierTarget(SkillArtifactApplicableProperty.ATTACK)
+                .build();
+        final HeroStatisticsIf heroStatistics = HeroStatistics.KNIGHT;
+        final int expected = (int) (heroStatistics.getAttack() + heroStatistics.getAttack() * MULTIPLIER);
+        // when
+        final HeroStatisticsIf result = artifactApplier.calculateHeroUpgradedStatisticsAfterApplyingArtifact(effect, heroStatistics);
+        // then
+        assertEquals(expected, result.getAttack());
+    }
+
+    @Test
+    void shouldUpgradeHeroStatisticsCorrectlyWhenAdding() {
+        // given
+        final ArtifactEffect<ArtifactEffectApplicable> effect = ArtifactEffect.builder()
+                .effectValue(BigDecimal.valueOf(VALUE_TO_ADD))
+                .effectApplyingMode(ArtifactApplyingMode.ADD)
+                .applierTarget(SkillArtifactApplicableProperty.ATTACK)
+                .build();
+        final HeroStatisticsIf heroStatistics = HeroStatistics.KNIGHT;
+        final int expected = (int) (heroStatistics.getAttack() + VALUE_TO_ADD);
+        // when
+        final HeroStatisticsIf result = artifactApplier.calculateHeroUpgradedStatisticsAfterApplyingArtifact(effect, heroStatistics);
+        // then
+        assertEquals(expected, result.getAttack());
     }
 }
