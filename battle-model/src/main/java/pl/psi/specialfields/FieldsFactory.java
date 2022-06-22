6f75118be631a74716709dcdb99d8cdfc32e932d
Jankaz2 <jkazmierczak.it@gmail.com>
2022-06-22 15:49:35 +0200

    modify logic of buffing creatures by special_fields, add HolyGround and EvilFog to FieldsFactory, fix tests

--- battle-model/src/main/java/pl/psi/specialfields/FieldsFactory.java
+++ battle-model/src/main/java/pl/psi/specialfields/FieldsFactory.java
@@ -8,23 +8,34 @@ public class FieldsFactory {
         switch (densityLevel) {
             case LOW:
                 return List.of(
-                    new CrackedIce(),
-                    new CloverField());
+                        new CrackedIce(),
+                        new CloverField(),
+                        new HolyGround(),
+                        new EvilFog());
             case MEDIUM:
                 return List.of(
-                    new CrackedIce(),
-                    new CloverField(),
-                    new CrackedIce(),
-                    new CloverField(),
-                    new CrackedIce());
+                        new CrackedIce(),
+                        new CloverField(),
+                        new CrackedIce(),
+                        new CloverField(),
+                        new CrackedIce(),
+                        new HolyGround(),
+                        new EvilFog(),
+                        new HolyGround(),
+                        new EvilFog());
             case HIGH:
                 return List.of(
-                    new CrackedIce(),
-                    new CloverField(),
-                    new CloverField(),
-                    new CloverField(),
-                    new CloverField(),
-                    new CloverField());
+                        new CrackedIce(),
+                        new CloverField(),
+                        new CloverField(),
+                        new CloverField(),
+                        new CloverField(),
+                        new CloverField(),
+                        new HolyGround(),
+                        new HolyGround(),
+                        new EvilFog(),
+                        new EvilFog(),
+                        new EvilFog());
             default:
                 return new ArrayList<>();
         }
